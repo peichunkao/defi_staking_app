a0f56bffdb0a48538a62e4e7713718318fc2d8ba
Steven Kao <stevenkaoo@gmail.com>
2022-04-29 21:54:25 +1000

    'Add truffle-config.js and Migrations.sol files'

---
+++ defi-staking-app/src/contracts/Migrations.sol
@@ -0,0 +1,23 @@
+pragma solidity >= 0.5.0;
+
+contract Migrations {
+    address public owner;
+    uint public last_completed_migration;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+    modifier restricated() {
+        if (msg.sender == owner) _;
+    }
+
+    function set_completed(uint completed) public restricated {
+        last_completed_migration = completed;
+    }
+
+    function upgrade(address new_address) public restricated {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.set_completed(last_completed_migration);
+    }
+}
